65c1dcddc825a0fe334489923fbbb7b1ff066f91
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2026-02-17 23:44:30 +0100

    added test route

--- routes/admin/auth.php
+++ routes/admin/auth.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
  * Prefix: /api/admin
  */
 
+// Test route to check if admin routes are working
+Route::get('/test', function() {
+    return response()->json(['message' => 'Admin routes are working!']);
+});
+
 Route::post('/login', [App\Http\Controllers\Admin\AuthController::class, 'login']);
 
 Route::post('/logout', [App\Http\Controllers\Admin\AuthController::class, 'logout'])->middleware('auth:api');
