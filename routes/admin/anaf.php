608d774e245463152d8d86a883cfbfbb5386f885
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2026-03-21 16:34:46 +0100

    get company

--- routes/admin/anaf.php
+++ routes/admin/anaf.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\AnafController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/anaf/company-lookup', [AnafController::class, 'lookupCompany']);
+Route::post('/anaf/getCompany', [AnafController::class, 'lookupCompany']);
