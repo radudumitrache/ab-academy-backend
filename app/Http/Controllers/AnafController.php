df2ba3d76ebf360d018b69b4acfb35673260a761
radudumitrache <radu.dumitrache@student.nhlstenden.com>
2026-03-23 22:16:26 +0100

    tried to fix the error

--- app/Http/Controllers/AnafController.php
+++ app/Http/Controllers/AnafController.php
@@ -12,24 +12,6 @@ class AnafController extends Controller
 
     public function lookupCompany(Request $request)
     {
-        $data = $request->validate([
-            'cui'  => 'required|integer',
-            'data' => 'required|date_format:Y-m-d',
-        ]);
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post('https://andreeaberkhout.oph.st/webhook-test/a1c92927-b2b9-48e6-864b-86a7a2022ed3', [
-            [
-                'cui'  => (int) $data['cui'],
-                'data' => $data['data'],
-            ],
-        ]);
-
-        if ($response->failed()) {
-            return $this->error('ANAF service unavailable', 502);
-        }
-
-        return $this->success($response->json(), 'Company data retrieved successfully');
+        return $this->success(null, 'endpoint called');
     }
 }
